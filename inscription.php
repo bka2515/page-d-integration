24a89c6e6a95ea741a8cec6031470d849f1f9531
bka2515 <63196741+bka2515@users.noreply.github.com>
2020-04-11 05:24:11 +0200

    Add files via upload

---
+++ inscription.php
@@ -0,0 +1,75 @@
+
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8" />
+      <link rel="stylesheet" href="cssinscription/inscription.css">
+      <style>
+      </style>
+   </head>
+   <body>
+      <div id="case">
+      <div id="tof">
+         
+         <div class="icon1">
+        <h2>Le plaisir de jouer</h2>
+            
+            
+        <img id="img0" src="images/logo-QuizzSA.png" alt="">
+         </div>
+<div id=part>
+              <div class="bande"><h5>CREER ET PARAMERTER VOS QUIZZ</h5>
+              <div class="bande1"><h3 style="margin-left: 50%;">AAA</h3> <br> 
+              <h3 style="margin-left: 58%; margin-top: -20%;">BBB</h3>
+              <img id="img2" src="images/avatar2.png" alt="">
+             
+      <nav id="liste">
+         <ul id="navigation">
+            <li id="LQ"><a href="#">Liste Questions </a>
+            <img id="img4" src="images/ic-liste.png" alt="">
+            </li>
+            <li id="LQ1"><a href="#">Créer Admin  </a>
+            <img id="img5" src="images/ic-ajout-active.png" alt="">
+            </li>
+            <li id="LQ2"><a href="#">Liste Joueurs</a>
+            <img id="img4" src="images/ic-liste.png" alt="">
+            </li>
+            <li id="LQ3"><a href="#">Créer Questions</a>
+            <img id="img6" src="images/ic-ajout.png" alt="">
+          </li>
+         </ul>
+      </nav> 
+    
+ 
+</div>
+            <input id="bt" type="submit" name="deconnect" placeholder="Deconnexion" value="Deconnexion"/>
+         </div>
+                <div id="champ"><h5>S'INSCRIRE</h5><br>
+                <p class="QZ"> pour proposer des quizz</p>
+                <hr>
+                <form method="post" action="">
+      <div class="prenom"> <p>Prenom</p>
+      </div>
+<input class="input1"  type="text" name="prenom" placeholder="PRENOM"/><br>
+<div class="nom"> <p>Nom</p></div>
+           <input  class="input2" type="text" name="nom" placeholder="NOM" value="$nom" /><br />
+<div class="login"> <p>login</p></div>
+         <input  class="input3" type="text" name="login" placeholder="LOGIN"  /><br />
+        <div class="pwd"> <p>Password</p></div>
+         <input  class="input4" type="text" name="pass" placeholder="PASS WORD" /><br />
+         <div class="pwd2"><p>Confirmer Password</p></div>
+         <input  class="input5" type="password" name="repass" placeholder="REPASS WORD" /><br />
+         <div class="avt"><h6>avatar</h6></div>
+         <input class="fil" type="file" name="fichier" placeholder="Choisir un fichier" /><br />
+         <input class="avatar" type="submit" name="valider" value="Créer compte" /><br id="HR1">
+         
+      </form>
+               
+                <img id="img1" src="images/Avatar.png" alt="">
+</div>
+</div>
+</div>
+</div>
+   </body>
+</html>
